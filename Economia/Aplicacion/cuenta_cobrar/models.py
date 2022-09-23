414c0fd228d7bab0c95db31a80989a5f7b4c158b
AndresM023 <andresmolina032@outlook.es>
2022-09-22 21:43:27 -0500

    Grabado

--- Economia/Aplicacion/cuenta_cobrar/models.py
+++ Economia/Aplicacion/cuenta_cobrar/models.py
@@ -10,7 +10,7 @@ meses_diferidos = [
 class Cabecera(models.Model):
     nombre= models.CharField(max_length=200, unique=True)
     deuda = models.DecimalField(default=0.00, max_digits=9, decimal_places=2, verbose_name='Monto a diferir')
-    fecha_cobro = models.DateField(blank=True, null=True)
+    fecha_cobro = models.DateField(blank=False, null=False,default=timezone.now)
     meses_a_diferir = models.CharField(max_length=200,choices=meses_diferidos,default=meses_diferidos[0][0])
     cuota_mensual = models.DecimalField(max_digits=7,decimal_places=2,verbose_name='Cuota Mensual',default=0.00)
 
@@ -27,7 +27,7 @@ class Cabecera(models.Model):
 class PagoDeuda(models.Model):
     cabecera = models.ForeignKey(Cabecera, on_delete=models.CASCADE, null=True, blank=True,verbose_name='Cliente',related_name='cab')
     abono = models.DecimalField(default=0.00, max_digits=9, decimal_places=2, verbose_name='Pago')
-    fecha_ab = models.DateField('Fecha de pago',default=timezone.now)
+    fecha_ab = models.DateField('Fecha de pago',default=timezone.now,null = False,blank = False)
     deuda = models.DecimalField(default=0.00, max_digits=9, decimal_places=2, verbose_name='Deuda')
 
 
